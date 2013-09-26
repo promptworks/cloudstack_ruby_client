1580f35b447dcc68312dc35e904175d1c99f8206
Len Smith <len@barrison.com>
2013-09-26 12:15:53 -0400

    Added IP Address Methods to Client

---
+++ lib/cloudstack_ruby_client/api/address_api.rb
@@ -0,0 +1,9 @@
+module CloudstackRubyClient
+  module Api
+    module Address
+      cmd_processor :associate_ip_address,
+                    :list_public_ip_addresses,
+                    :disassociate_ip_address
+    end
+  end
+end
